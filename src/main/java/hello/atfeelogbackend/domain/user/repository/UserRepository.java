1f2f9620055c6e11f46b4d058cb87380d486a701
nadaa <nadaa@ajou.ac.kr>
2026-08-14 00:06:54 +0900

    feat: 이메일 & 닉네임 중복체크 쿼리

--- src/main/java/hello/atfeelogbackend/domain/user/repository/UserRepository.java
+++ src/main/java/hello/atfeelogbackend/domain/user/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package hello.atfeelogbackend.domain.user.repository;
 
 import hello.atfeelogbackend.domain.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(long id);
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByName(String name);
 }
